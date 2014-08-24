2ad3e9d98c42b23abd733c9e3c2f05d507acd9a0
Luke Iannini <lukexi@me.com>
2014-08-24 02:32:49 -0700

    Adding CrayonCannon test and scenekrift submodule

---
+++ CrayonCannon/CrayonCannon/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  CrayonCannon
+//
+//  Created by Luke Iannini on 8/24/14.
+//  Copyright (c) 2014 tree. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
